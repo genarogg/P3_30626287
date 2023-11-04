f6cc815e067d6b10cbca6a6c8df31e48c37327a2
genaro gonzalez <genarrogg@gmail.com>
2023-11-04 15:34:29 -0400

    sa

---
+++ models/admin.js
@@ -0,0 +1,153 @@
+const db = require ('../database/conection');
+
+module.exports = {
+    obtener() {
+        return new Promise ((resolve, reject) =>{
+            const sql = `SELECT productos.*, categorias.nombre, imagenes.url, imagenes.destacado
+            FROM productos
+            INNER JOIN categorias
+            ON productos.categoria_id = categorias.id
+            INNER JOIN imagenes
+            ON productos.id = imagenes.producto_id`;
+            db.all (sql, (err, resultados) =>{
+                if (err) reject (err);
+                else resolve (resultados);
+            });
+        });
+    },
+    obtenerctg(){
+        return new Promise ((resolve, reject) =>{
+            const sql = 'Select * FROM categorias';
+            db.all (sql, (err, resultados) =>{
+                if (err) reject(err);
+                else resolve(resultados);
+            });
+        });
+    },
+    obtenerctgPorId(id){
+        return new Promise ((resolve, reject) => {
+            const sql = 'SELECT * FROM categorias where id = ?'
+            db.get(sql, [id], (err, resultados)=>{
+                if (err) reject(err);
+                else resolve(resultados);
+            });
+        });
+    },
+    insertarctg(nombre){
+        return new Promise ((resolve, reject) =>{
+            const sql = 'INSERT INTO categorias (nombre) VALUES (?)';
+            db.run(sql, [nombre], (err, resultados) => {
+                if (err) reject(err);
+                else resolve(resultados);
+            });
+        });
+    },
+    actualizarctg(nombre, id){
+        return new Promise ((resolve, reject)=>{
+            const sql = 'UPDATE categorias SET nombre = ? WHERE id = ?'
+            db.run(sql, [nombre, id], (err) =>{
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+    },
+    eliminarctg(id){
+        return new Promise ((resolve, reject)=>{
+            const sql = 'DELETE FROM categorias WHERE id = ?'
+            db.run(sql, [id], (err)=>{
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+    },
+    obtenerprd(){
+        return new Promise ((resolve, reject) =>{
+            const sql = 'SELECT * from productos';
+            db.all (sql, (err, resultados) =>{
+                if (err) reject(err);
+                else resolve(resultados);
+            });
+        });
+    },
+    obtenerprdPorId(id){
+        return new Promise ((resolve, reject)=>{
+            const sql = 'SELECT * FROM productos WHERE id = ?';
+            db.get(sql, [id], (err, resultados)=>{
+                if (err) reject(err);
+                else resolve(resultados);
+            });
+        });
+    },
+    insertarprd(nombre, precio, codigo, descripcion, marca, edad, categoria_id){
+        return new Promise ((resolve, reject) =>{
+            const sql = 'INSERT INTO productos (nombre, precio, codigo, descripcion, marca, edad, categoria_id) VALUES (?, ?, ?, ?, ?, ?, ?)';
+            db.run (sql, [nombre, precio, codigo, descripcion, marca, edad, categoria_id], (err, resultados)=>{
+                if (err) reject(err);
+                else resolve(resultados);
+            });
+        });
+    },
+    actualizarprd(nombre, precio, codigo, descripcion, marca, edad, id){
+        return new Promise ((resolve, reject)=>{
+            const sql = 'UPDATE productos SET nombre = ?, precio = ?, codigo = ?, descripcion = ?, marca = ?, edad = ? WHERE id = ?';
+            db.run(sql, [nombre, precio, codigo, descripcion, marca, edad, id], (err)=>{
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+    },
+    eliminarprd(id){
+        return new Promise ((resolve, reject)=>{
+            const sql = 'DELETE FROM productos WHERE id = ?';
+            db.run(sql, [id], (err)=>{
+                if (err) reject(err);
+                else resolve();
+            });
+        });        
+    },
+    obtenerimg(){
+        return new Promise ((resolve, reject) =>{
+            const sql = 'SELECT * FROM imagenes';
+            db.all(sql, (err, resultados)=>{
+                if (err) reject(err);
+                else resolve(resultados);
+            });
+        });
+    },
+    obtenerimgPorId(id){
+        return new Promise ((resolve, reject)=>{
+            const sql = 'SELECT * FROM imagenes WHERE id = ?';
+            db.get(sql, [id], (err, resultados)=>{
+                if (err) reject(err);
+                else resolve(resultados);
+            });
+        });
+    },
+    insertarimg(url, destacado, producto_id){
+        return new Promise ((resolve, reject)=>{
+            const sql = 'INSERT INTO imagenes (url, destacado, producto_id) VALUES (?, ?, ?)';
+            db.run (sql, [url, destacado, producto_id], (err, resultados) =>{
+                if (err) reject(err);
+                else resolve(resultados);
+            });
+        });
+    },
+    actualzarimg(url, destacado, id){
+        return new Promise ((resolve, reject)=>{
+            const sql = 'UPDATE imagenes SET url = ?, destacado = ? WHERE id = ?';
+            db.run (sql, [url, destacado, id], (err)=>{
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+    },
+    eliminarimg(id){
+        return new Promise ((resolve, reject)=>{
+            const sql = 'DELETE FROM imagenes WHERE id = ?';
+            db.run(sql, [id], (err)=>{
+                if (err) reject (err);
+                else resolve();
+            });
+        });
+    },
+};
